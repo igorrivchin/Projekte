bbfdddd69e4d5a1d76d382264ada068556973567
Rivchin <Igor.Rivchin@adesso.de>
2020-12-01 23:19:06 +0100

    ui logo and navigation tabs

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -38,57 +38,111 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-        //statt Scaffold
-        color: Colors.yellow,
-        home: DefaultTabController(
+    return MaterialApp(
+      //statt Scaffold
+      color: Colors.yellow,
+      home: SafeArea(
+        child: DefaultTabController(
           length: 3,
           child: new Scaffold(
-            body: TabBarView(
-              children: [
-                new Container(
-                  color: Colors.yellow,
-                ),
-                new Container(
-                  color: Colors.orange,
-                ),
-                new Container(
-                  //color: Colors.lightGreen,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                          'assets/images/TSC_Hansa_50_Logo_klein.jpg'),
-                      fit: BoxFit.cover,
+            body: Stack(
+              children: <Widget>[
+                TabBarView(
+                  children: [
+                    new Container(
+                      color: Colors.grey,
                     ),
+                    new Container(
+                      color: Colors.blueGrey,
+                    ),
+                    new Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/TSC_Hansa_50_Logo_klein.jpg'),
+                          fit: BoxFit.contain,
+                          //  colorFilter: new ColorFilter.mode( Colors.blue.withOpacity(0.5), BlendMode.dst)
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30)),
+                    color: Colors.purple,
                   ),
-                ),
+                )
               ],
             ),
-            //bottomNavigationBar: new TabBar(
-            appBar: new TabBar(
-              tabs: [
-                Tab(
-                  icon: new Icon(Icons.home),
-                ),
-                Tab(
-                  icon: new Icon(Icons.rss_feed),
-                ),
-                Tab(
-                  //icon: new Icon(Icons.perm_identity),
-                  icon: new Icon(Icons.calendar_today_rounded),
-                ),
-              ],
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.blue,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorPadding: EdgeInsets.all(5.0),
-              indicatorColor: Colors.red,
+            appBar: new AppBar(
+              elevation: 0,
+              title: new TabBar(
+                tabs: [
+                  Tab(
+                    icon: new Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.calendar_today_rounded),
+                  ),
+                ],
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+              ),
+              backgroundColor: Colors.black,
             ),
-            backgroundColor: Colors.black,
-          ), //scaffold end
+          ),
         ),
       ),
     );
   }
 }
+
+/*
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+              labelColor: Colors.white,
+              unselectedLabelColor: Colors.black,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorPadding: EdgeInsets.all(5.0),
+            ),
+            title: Text('Todo App'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
